441b25ad50af2b23223fe86873a3ca957babaf51
sakshamg567 <saksham060306@gmail.com>
2025-02-13 13:04:06 +0530

    barebones sign-in page

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 const AuthLayout = () => {
-  return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
-  )
+   return (
+      <>
+         <Stack>
+            <Stack.Screen 
+               name='sign-in'
+               options={{headerShown: false }}
+            />
+         </Stack>
+         <StatusBar backgroundColor={'#ffffff'}/>
+      </>
+   )
 }
 
 export default AuthLayout
