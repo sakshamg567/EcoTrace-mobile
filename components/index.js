1d4ca184290909bbd9627599edf93c9a54487799
sakshamg567 <saksham060306@gmail.com>
2025-02-14 12:17:33 +0530

    Refactor component imports and add new components

--- components/index.js
+++ components/index.js
@@ -1,7 +1,15 @@
 import FormField from "./FormField";
-
+import EcoScoreDisplay from './EcoScoreDisplay';
+import CarbonEmissionsChart from './CarbonEmissionsChart';
+import EcoActionsCarousel from './EcoActionsCarousel';
+import TodayMetrics from './TodayMetrics';
 
 
 export {
   FormField,
+  EcoActionsCarousel,
+  EcoScoreDisplay,
+  TodayMetrics,
+  CarbonEmissionsChart
 };
+``
